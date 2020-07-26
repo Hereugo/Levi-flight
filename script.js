39174030c9895009c023b526413293bd8232edf2
Hereugo <thereug6@gmail.com>
2020-07-26 20:53:35 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+var pos , prev
+
+function setup() {
+	createCanvas(400 , 400)
+	background(0)
+	pos = createVector(200 , 200)
+	prev = pos.copy()
+}
+
+function draw() {
+	stroke(255)
+	strokeWeight(2)
+	line(prev.x , prev.y , pos.x , pos.y)
+
+	var step = p5.Vector.random2D()
+	
+	var r = random(100)
+	if (r <= 2) 
+		step.setMag(random(25 , 50))
+	else
+		step.setMag(2)
+
+	prev.set(pos)
+	pos.add(step)
+}
